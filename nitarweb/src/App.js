3846d2dc0fbfe248e3941c0ac0fbc4dd8dfde605
Won Maung Thein <wonmaungthein@gmail.com>
2018-09-07 22:44:11 +0100

    added draft files

--- nitarweb/src/App.js
+++ nitarweb/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import bagIMG from "./bag.JPG";
 
+const imgStyle = {
+  width: 350,
+  height: 350
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to NeNe's Website</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Welcome to GAW Website</h1>
+          <h2>Girl's Awesome Wears</h2>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h3>
+            <b>Hello Fashion lovers!!!! Welcome to my new website.</b>
+          </h3>
+          <div className="img-container">
+            <img src={bagIMG} style={imgStyle} alt="bap img" />
+            <div className="img-discription">
+              <h2>Bag Title</h2>
+              <p>
+                Bag balalalala ေဒအိပ္ သည္ ကမာမွာ အေကာင္းဆံုး အိပ္ ၿဖစ္ ပါ သိည္။
+                ေနြ ၊ မိုး ၊ ေအာင္း ရာသီ မေရြး သံုး စြဲ နိုင္ ပါသည္။{" "}
+              </p>
+              <button>Next</button>
+              <button>Previous</button>
+            </div>
+          </div>
         </p>
+        <footer className="App-footer">
+          <li>Social Icons</li>
+        </footer>
       </div>
     );
   }
